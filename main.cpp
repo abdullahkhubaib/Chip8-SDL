2bbb6c09a8c6c1126096981a55e50d54e25de91c
abdullahkhubaib <59149412+abdullahkhubaib@users.noreply.github.com>
2023-05-01 15:45:37 -0600

    Capped FPS to 60

--- main.cpp
+++ main.cpp
@@ -6,12 +6,19 @@
 
 
 int main(int argc, char* args[]) {
+    const int frameDelay = 1000 / 60;
+    uint32_t frameStart = 0;
+    uint32_t frameTime = 0;
     chip8 c8(args[1]);
     while(c8.running) {
+        frameStart = SDL_GetTicks();
         c8.handleEvents();
         c8.update();
         c8.render();
         SDL_Delay(10);
+        frameTime = SDL_GetTicks() - frameStart;
+        if(frameDelay > frameTime)
+            SDL_Delay(frameDelay - frameTime);
     }
 
     return 0;
