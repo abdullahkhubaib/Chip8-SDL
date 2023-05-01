941bd7ec93108f458e19d910ddcc2ef54be66c94
abdullahkhubaib <59149412+abdullahkhubaib@users.noreply.github.com>
2023-04-30 22:15:36 -0600

    Added 0-F character sprites

--- include/chip8.h
+++ include/chip8.h
@@ -8,19 +8,19 @@
 
 class chip8 {
 private:
-    std::array<uint8_t, 16> reg{};
-    uint16_t index{};
-    uint16_t pc{};
-    std::array<uint8_t, 64> stack{};
-    uint8_t sp{};
-    uint8_t dt{};
-    uint8_t st{};
-    std::array<uint64_t, 32> frame_buffer{};
-    std::array<uint8_t, 4096> mem{};
+    std::array<uint8_t, 16> reg;
+    uint16_t index;
+    uint16_t pc;
+    std::array<uint8_t, 64> stack;
+    uint8_t sp;
+    uint8_t dt;
+    uint8_t st;
+    std::array<uint64_t, 32> frame_buffer;
+    std::array<uint8_t, 4096> mem;
     static const uint8_t characters[16][5];
-    bool running{};
-    SDL_Window* window{};
-    SDL_Renderer* renderer{};
+    bool running;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
 public:
     chip8();
     ~chip8();
